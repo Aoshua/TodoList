db33fefb300e2c0fca587bcc643376dba4ac136a
Aoshua <64.josh@gmail.com>
2020-06-25 16:34:57 -0600

    General layout for add view

---
+++ TodoList/AddViewController.swift
@@ -0,0 +1,32 @@
+//
+//  AddViewController.swift
+//  TodoList
+//
+//  Created by Joshua Abbott on 6/25/20.
+//  Copyright © 2020 Joshua Abbott. All rights reserved.
+//
+
+import UIKit
+
+class AddViewController: UIViewController {
+    
+    @IBOutlet weak var txtTitle: UITextField!
+    @IBOutlet weak var txtDescription: UITextView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
